14c6b4644edf0c6d3fec6d12947650e0dc1c7215
Ross Sanderson <ross.h.sanderson@gmail.com>
2024-01-26 11:39:25 +1100

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-green-400">Welcome to SvelteKit, by Rosscoe  doing an update her</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read tation</p>
